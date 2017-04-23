bdc04c82912acc880abbf1f0cdd332b486f00005
ben.greer <ben.greer@service-now.com>
2017-04-23 08:07:38 -0700

    upgrade to cassowry

--- src/Array/Vector.elm
+++ src/Array/Vector.elm
@@ -6,50 +6,75 @@ module Array.Vector
         , isEmpty
         , initialize
         , fromList
+        , toList
         , foldl
         , foldr
         , map
         , filter
         , get
-        , set
+        , nth
+        , update
         , take
         , drop
+        , slice
+        , push
+        , append
+        , appendAll
+        , prepend
+        , range
+        , times
+        , repeat
+        , all
+        , any
+        , insertAt
+        , removeAt
+        , remove
+        , sort
+        , member
+        , toString
         )
 
-import Native.Rbbit
+import Native.Rrbit
 
-type Vector itemType = Vector itemType
+type Vector itemType = Vec itemType
+--Vector : Vector itemType
+
+--type Collection itemType = Vector itemType | List itemType
 
 length : Vector itemType -> Int
 length vec = 
-    Native.Rbbit.length vec
+    Native.Rrbit.length vec
 
 empty : Vector itemType
 empty = 
-    Native.Rbbit.empty
+    Native.Rrbit.empty
 
 isEmpty : Vector itemType -> Bool
 isEmpty vec = 
     length vec == 0
 
 
-initialize : (Int -> itemType) -> Int -> Vector itemType
-initialize fn size = 
-    Native.Rbbit.initialize fn size
+initialize : Int -> (Int -> itemType) -> Vector itemType
+initialize size fn =
+    Native.Rrbit.initialize size fn
 
+--fromListHelp value vec
 
 fromList : List itemType -> Vector itemType
 fromList list =
-    empty
+    List.foldl append empty list
 
+toList : Vector itemType -> List itemType
+toList vec =
+    foldr (::) [] vec
 
 foldl : (a -> b -> b) -> b -> Vector a -> b
 foldl fn rrb = 
-    Native.Rbbit.foldl fn rrb
+    Native.Rrbit.foldl fn rrb
 
 foldr : (a -> b -> b) -> b -> Vector a -> b
 foldr fn rrb = 
-    Native.Rbbit.foldr fn rrb
+    Native.Rrbit.foldr fn rrb
 
 map : (a -> b) -> Vector a -> Vector b
 map fn rrb =
@@ -62,7 +87,7 @@ filter fn rrb =
 nth : Int -> Vector itemType -> Maybe itemType
 nth i vec =
     let
-        res = Native.Rbbit.nth i vec Nothing
+        res = Native.Rrbit.nth i vec Nothing
     in
         case res of
             Nothing ->
@@ -75,63 +100,98 @@ get : Int -> Vector itemType -> Maybe itemType
 get i rrb =
     nth i rrb
 
-set : Int -> itemType -> Vector itemType -> Vector itemType
-set i value rrb =
-    Native.Rbbit.update i value rrb
+update : Int -> itemType -> Vector itemType -> Vector itemType
+update i value vec =
+    Native.Rrbit.update i value vec
 
 take : Int -> Vector itemType -> Vector itemType
-take n rrb =
-    Native.Rbbit.take n rrb
+take n vec =
+    Native.Rrbit.take n vec
 
 drop : Int -> Vector itemType -> Vector itemType
-drop n rrb =
-    Native.Rbbit.drop n rrb
+drop n vec =
+    Native.Rrbit.drop n vec
+
+slice : Int -> Int -> Vector a -> Vector a
+slice from to vec =
+    drop from (take (to - 1) vec)
+
+prepend : itemType -> Vector itemType -> Vector itemType
+prepend t vec =
+    Native.Rrbit.prepend t vec
 
 append : itemType -> Vector itemType -> Vector itemType
 append t vec = 
-    Native.Rbbit.append t vec
+    Native.Rrbit.append t vec
 
-appendAll : Vector itemType -> Vector itemType | List itemType -> Vector itemType
-appendAll vec tail = 
-    case tail of 
-        List ->
-            appendAllDirect vec (fromList tail)
-        Vector ->
-            appendAllDirect vec tail
+push : itemType -> Vector itemType -> Vector itemType
 
+push = append
 
-appendAllDirect : Vector itemType -> Vector itemType -> Vector itemType
-appendAllDirect left right =
-    Native.Rbbit.appendAll left right
+appendAll : Vector itemType -> Vector itemType -> Vector itemType
+appendAll left right =
+    Native.Rrbit.appendAll left right
 
--- range : Int -> Int -> Vector itemType
--- range from to =
--- times : Int -> (Int -> itemType) -> Vector itemType
+range : Int -> Int -> Vector itemType
+range from to =
+    Native.Rrbit.range from to
+
+times : Int -> (Int -> itemType) -> Vector itemType
 times end populate =
-	let
-		help i end fn list =
-			if i < end then
-				help (i + 1) end (append (fn i) list)
-			else
-				list
-	in
-		help 0 end populate empty
+    timesHelp populate 0 end empty
 
--- every
--- any
--- sort
+repeat : Int -> itemType -> Vector itemType
+repeat end item =
+    timesHelp (\_ -> item) 0 end empty
+
+timesHelp : (Int -> itemType) -> Int -> Int -> Vector itemType -> Vector itemType
+timesHelp fn i end list =
+    if i >= end then
+        list
+    else
+        let
+            value = fn i
+            list_ = append value list
+        in
+            timesHelp fn (i + 1) end list_
+
+
+all : (itemType -> Bool) -> Vector itemType -> Bool
+all fn vec =
+    Native.Rrbit.every fn vec
+
+any : (itemType -> Bool) -> Vector itemType -> Bool
+any fn vec =
+    Native.Rrbit.some fn vec
+
+removeAt : Int -> Vector itemType -> Vector itemType
+removeAt i vec =
+    Native.Rrbit.removeAt i vec
+
+remove : itemType -> Vector itemType -> Vector itemType
+remove value vec =
+    Native.Rrbit.remove value vec
+
+insertAt : Int -> Vector itemType -> Vector itemType
+insertAt i vec =
+    Native.Rrbit.insertAt i vec
+    
+sort : Vector itemType -> Vector itemType
+sort vec =
+    Native.Rrbit.sort vec
+
+member : itemType -> Vector itemType -> Bool
+member value vec =
+    Native.Rrbit.includes value vec
+
+toString : Vector itemType -> String
+toString vec =
+    "not supported" ++ "...yet"
+
 -- intersperse
 -- unique : Vector itemType - > Vector itemType
 -- difference : Vector itemType - > Vector itemType
 -- flatten : Vector itemType - > Vector itemType
 -- flattenDeep : Vector itemType - > Vector itemType
--- findIndexOf : itemType -> Vector itemType -> Maybe Bool
--- mapAndFlatten : (T -> List itemType | Vector itemType) -> Vector itemType -> Vector
--- mapAndFlatten fn vec =
-    -- Native.Rrbit.reduce (mapAndFlatten fn) empty
 
--- _mapAndFlatten : (T -> List itemType | Vector itemType) -> Vector itemType -> itemType -> Vector itemType
--- _mapAndFlatten fn acc value =
-    -- case value of 
-        -- List ->
 
